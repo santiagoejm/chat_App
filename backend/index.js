646f45df7d0d36a8b7b4aa757cf891dd237274ba
Santiago Julian <sjulian983@gmail.com>
2022-04-26 17:50:20 +0200

    connect and disconnect events

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,14 @@ const io = new Server(server, {
   },
 });
 
+io.on("connection", (socket) => {
+  console.log(socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
 server.listen(3001, () => {
   console.log("Server Runnning on port 3001...");
 });
