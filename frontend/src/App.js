b524c34d91a5b06e284c5705baeab513dc45bf3a
Santiago Julian <sjulian983@gmail.com>
2022-04-27 16:43:23 +0200

    creating and controlling name & room values

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,31 @@
 import "./App.css";
 import io from "socket.io-client";
+import { useState } from "react";
 
 const socket = io.connect("http://localhost:3001");
 
 function App() {
-  return <div className="App">Chat App</div>;
+  const [userName, setUserName] = useState("");
+  const [room, setRoom] = useState("");
+
+  const joinRoom = () => {};
+
+  return (
+    <div className="App">
+      <h3>Join a Chat</h3>
+      <input
+        type="text"
+        placeholder="Name..."
+        onChange={(e) => setUserName(e.target.value)}
+      ></input>
+      <input
+        type="text"
+        placeholder="Room n°"
+        onChange={(e) => setRoom(e.target.value)}
+      ></input>
+      <button>Join a Room</button>
+    </div>
+  );
 }
 
 export default App;
