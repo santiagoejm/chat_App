cffb45d23f4368b4ffa43201002373c928c2f129
Santiago Julian <sjulian983@gmail.com>
2022-04-28 18:44:45 +0200

    Chat structure

--- frontend/src/Components/Chat.jsx
+++ frontend/src/Components/Chat.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Chat = ({ socket, userName, room }) => {
-  return <div>Chat</div>;
+  return (
+    <div>
+      <div className="chat-header">
+        <p>LIVE CHAT</p>
+      </div>
+      <div className="chat-body"></div>
+      <div className="chat-footer">
+        <input type="text" placeholder="Hey..." />
+        <button>&#9658;</button>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
