0257134fdf3ea865b5e8e414be90fffb05843992
Santiago Julian <sjulian983@gmail.com>
2022-04-29 16:57:43 +0200

    chat component stating message

--- frontend/src/Components/Chat.jsx
+++ frontend/src/Components/Chat.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Chat = ({ socket, userName, room }) => {
+  const [currentMessage, setCurrentMessage] = useState("");
+
+  const sendMessage = async () => {
+    if (currentMessage !== "") {
+      const messageData = {
+        room: room,
+        userName: userName,
+        message: currentMessage,
+        time: `${new Date(Date.now()).getHours()}:${new Date(
+          Date.now()
+        ).getMinutes()}`,
+      };
+    }
+  };
+
   return (
     <div>
       <div className="chat-header">
@@ -8,7 +23,11 @@ const Chat = ({ socket, userName, room }) => {
       </div>
       <div className="chat-body"></div>
       <div className="chat-footer">
-        <input type="text" placeholder="Hey..." />
+        <input
+          type="text"
+          placeholder="Hey..."
+          onChange={(e) => setCurrentMessage(e.target.value)}
+        />
         <button>&#9658;</button>
       </div>
     </div>
