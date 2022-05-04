a383e2e32480299fc2766a2983ba8c90a9814b4b
Santiago Julian <sjulian983@gmail.com>
2022-05-04 13:53:25 +0200

    handling onKeyPress to send message

--- frontend/src/Components/Chat.jsx
+++ frontend/src/Components/Chat.jsx
@@ -56,6 +56,9 @@ const Chat = ({ socket, userName, room }) => {
           type="text"
           placeholder="Hey..."
           onChange={(e) => setCurrentMessage(e.target.value)}
+          onKeyPress={(e) => {
+            e.key === "Enter" && sendMessage();
+          }}
         />
         <button onClick={sendMessage}>&#9658;</button>
       </div>
